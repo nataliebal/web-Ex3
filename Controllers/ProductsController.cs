57efa42fc476477caee805128693204d0f67d6cb
natalie balulu <natalie.balulu@live.biu.ac.il>
2019-05-28 21:48:30 +0300

    location

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -9,7 +9,7 @@ namespace WebApplication1.Controllers
     public class ProductsController : Controller
     {
         // GET: Products
-        public ActionResult Index()
+        public ActionResult display()
         {
             return View();
         }
